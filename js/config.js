0ea9af866a0829d23341ef4a6496422da7e208d3
𝐌 𝐈 𝐙 𝐔 𝐌 𝐀 𝐑 𝐔 <mizumaruofficialdump@gmail.com>
2025-12-07 17:36:55 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -10,42 +10,12 @@ window.scriptData = [
   },
 
   {
-    id: "sab_script",
-    title: "MZZHUB [SAB]",
-    url: "https://raw.githubusercontent.com/mizu-dump/DumpScript/refs/heads/main/loadser.lua",
-    description: "Script Brainrot Sniper, ESP, Desync For Steal A Brainrot",
-    category: "Helper",
-    tags: ["sab", "autosteal", "esp", "desync"],
-    image: "https://tr.rbxcdn.com/180DAY-b6ad9f8a2edddee0365eed4f19a8caf9/768/432/Image/Webp/noFilter"
-  },
-
-  {
-    id: "99_script",
-    title: "MZZHUB [99NITF]",
-    url: "https://raw.githubusercontent.com/mizu-dump/DumpScript/refs/heads/main/loadser.lua",
-    description: "Script Mob ESP, Auto Farm Days, Diamonds, Bring For 99 Nights In The Forest",
-    category: "Survival",
-    tags: ["forest", "night", "survival", "esp"],
-    image: "https://tr.rbxcdn.com/180DAY-643b9b9285f3bd9b63cf392db12a4d87/768/432/Image/Webp/noFilter"
-  },
-
-  {
-    id: "pvb_script",
-    title: "MZZHUB [PVB]",
-    url: "https://raw.githubusercontent.com/mizu-dump/DumpScript/refs/heads/main/loadser.lua",
-    description: "Script Spawner For Plants Vs Brainrots",
-    category: "Spawner",
-    tags: ["pvb", "plants", "brainrot", "spawner"],
+    id: "pvb_lgwxw",
+    title: "LGWXW [PVB]",
+    url: "https://raw.githubusercontent.com/mizu-dump/DumpScript/refs/heads/main/Legitxwx.lua%20(1).txt",
+    description: "Script Auto Farm, Miscellaneous For Plants Vs Brainrots",
+    category: "AUTO",
+    tags: ["pvb", "plants", "brainrot", "legitwxw"],
     image: "https://tr.rbxcdn.com/180DAY-a933a9ba765efea4061f4a101f18edd2/768/432/Image/Webp/noFilter"
-  },
-
-  {
-    id: "slap_script",
-    title: "MZZHUB [SLPTWR]",
-    url: "https://raw.githubusercontent.com/mizu-dump/DumpScript/refs/heads/main/loadser.lua",
-    description: "Script Auto Slap, Anti Void, Slap Aura, Unlock Gamepass For Slap Tower",
-    category: "Combat",
-    tags: ["slap", "gamepass", "troll", "slapaura"],
-    image: "https://tr.rbxcdn.com/180DAY-d02a1823db10e3dd0357dd1ba10f53d8/768/432/Image/Webp/noFilter"
   }
 ];
